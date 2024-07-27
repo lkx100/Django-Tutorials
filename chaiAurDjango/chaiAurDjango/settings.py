d2c0690822efe495f8c4a1e292f8549e2239831e
lkx100 <lk5999950@gmail.com>
2024-07-27 20:08:23 +0530

    creating & adding app

--- chaiAurDjango/chaiAurDjango/settings.py
+++ chaiAurDjango/chaiAurDjango/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# Mention apps that you start / create such that ur project recognises it.
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chai',
 ]
 
 MIDDLEWARE = [
